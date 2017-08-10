841133131ae231d143c99d34c1e1f2979d2eb28b
victorrrru <942021266@qq.com>
2017-08-10 16:36:45 +0800

    steam and lambda

--- src/test/java/com/example/demo/steamTest/SteamTest1.java
+++ src/test/java/com/example/demo/steamTest/SteamTest1.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,11 +17,15 @@ public class SteamTest1 {
 
     @Test
     public void test1() {
-        Stream<Long> stream = Stream.generate(new NatureSupplier());
-        stream.map((x)->{
-            return Math.tan(x) * x;
-                }
-        ).limit(20).forEach(System.out :: println);
+        Stream<Long> stream = Stream.generate(new Supplier<Long>() {
+            long a = 0;
+            @Override
+            public Long get() {
+                this.a = this.a + 1;
+                return a;
+            }
+        });
+        stream.map(x->Math.tan(x) * x).limit(20).forEach(System.out :: println);
          //如果想取得数列的第20~30项
 //        stream.skip(20).limit(10).collect(Collectors.toSet()).forEach(System.out :: println);
     }
@@ -36,17 +42,86 @@ public class SteamTest1 {
         //新建一个交易的集合
         List<Transaction> transactions= Arrays.asList(
                 new Transaction(xiaoming, 2017, 300),
-                new Transaction(xiaohong,2016,1000),
-                new Transaction(xiaohong,2017,400),
-                new Transaction(xiaohei,2016,710),
-                new Transaction(xiaohei,2016,700),
-                new Transaction(xiaobai,2016,950)
+                new Transaction(xiaohong, 2016,1000),
+                new Transaction(xiaohong, 2017,400),
+                new Transaction(xiaohei, 2016,710),
+                new Transaction(xiaohei, 2016,700),
+                new Transaction(xiaobai, 2016,950)
         );
 
         List<Transaction> stream = transactions.stream().filter(t -> t.getYear() == 2016)
-                .sorted(Comparator.comparing(transaction -> transaction.getValue()))
+                .sorted(Comparator.comparing(a -> a.getValue()))
                 .collect(Collectors.toList());
         System.out.println(stream);
+
+    }
+
+    @Test
+    public void test3() {
+        Stream<String> str = Stream.of("jsfiehguihr");
+        str.map(s -> s.toCharArray()).forEach(System.out :: println);
+    }
+
+    @Test
+    public void test4() {
+//        Stream.iterate(1,s -> s+1).limit(10).forEach(System.out :: println);
+        Stream.iterate(1,s -> s+1).peek(x->Math.tan(x)).limit(10).forEach(System.out :: println);
+    }
+
+    @Test
+    public void test5() {
+//        List<Integer> list = Arrays.asList(1, 2, 3, 5);
+//        System.out.println(list.stream().reduce(2,(sum, i) -> sum + i));
+        List<String> list1 = Arrays.asList("1", "2", "3", "5");
+        System.out.println(list1.stream().reduce(0, (sum, word) -> sum + word.length(),
+                (sum1, sum2) -> sum1 + sum2));
+    }
+
+    @Test
+    public void test6() {
+        List<Integer> numList = Arrays.asList(1, 1, null, 2, 3, 4, null, 5, 6, 7, 8, 9, 10);
+//        List<Integer> numsWithoutNullList = numList.stream()
+//                .filter(num -> num != null)
+//                .collect(() -> new ArrayList<Integer>(),
+//                        (list, item) -> list.add(item),
+//                        (list1, list2) -> list1.addAll(list2));
+
+//        List<Integer> l = numList.stream()
+//                .filter(num -> num != null)
+//                .collect(ArrayList :: new, ArrayList :: add, ArrayList :: addAll);
+
+        List<Integer> l = numList.stream().
+                filter(num -> num != null)
+                .collect(Collectors.toList());
+        System.out.println(l);
+    }
+
+    /**
+     * 對Collectors.toMap进行剖析
+     */
+    @Test
+    public void test7() {
+        //建立对象
+        Trader xiaoming=new Trader("小明","广州");
+        Trader xiaohong=new Trader("小红","广州");
+        Trader xiaohei=new Trader("小黑","广州");
+        Trader xiaobai=new Trader("小白","肇庆");
+
+        //新建一个交易的集合
+        List<Transaction> transactions= Arrays.asList(
+                new Transaction(xiaoming, 2017, 300),
+                new Transaction(xiaohong, 2016,1000),
+                new Transaction(xiaohong, 2017,400),
+                new Transaction(xiaohei, 2016,710),
+                new Transaction(xiaohei, 2016,700),
+                new Transaction(xiaobai, 2016,950)
+        );
+//        transactions.stream().collect(Collectors.toMap(Trader :: getName, Transaction ::
+//        getValue))
+        Map<String, Transaction> map = transactions.stream().collect(Collectors.toMap(transaction->transaction.getTrader().getName(), transaction->transaction, (existValue, newValue)->existValue));
+        for (Map.Entry<String, Transaction> entry:map.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue().getValue());
+        }
 
     }
 }
